7b12302a1ba9f9a83e1e051bdcffed0167fc8875
Franco Yunes <franco.yunes@hotmail.com>
2022-10-12 16:44:29 -0300

    :sparkles: Changed var name

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,17 +5,17 @@ import { useParams } from "react-router-dom";
 const Book = () => {
   const booksList = useSelector((state) => state.books);
   const bookId = useParams().id;
-  const buscarlibro = booksList.find(
+  const findBook = booksList.find(
     (book) => Number(book.isbn) === Number(bookId)
   );
   return (
-    buscarlibro && (
+    findBook && (
       <div>        
-        <div key={buscarlibro.isbn} className="col-lg-4 col-md-6 col-12 mb-3">
+        <div key={findBook.isbn} className="col-lg-4 col-md-6 col-12 mb-3">
           <div className="cards h-100 d-flex flex-column justify-content-center">
-            <h2>{buscarlibro.title}</h2>
-            <h3>{buscarlibro.author}</h3>
-            <img className="bookImg mt-auto" src={buscarlibro.imageLink} />
+            <h2>{findBook.title}</h2>
+            <h3>{findBook.author}</h3>
+            <img className="bookImg mt-auto" src={findBook.imageLink} />
           </div>
         </div>
       </div>
